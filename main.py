78e7d8bd948f9fd70518a5938ed88a74d2b0c2a7
felixgyabaa <gyabaafelix01@gmail.com>
2024-06-11 14:36:52 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,98 @@
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+
+# %%
+url = "/Users/felixgyabaa/Documents/data_analysis/datasets/automobile/imports-85.data"
+
+df = pd.read_csv(url, header=None)
+
+# %%
+df.head()
+df.tail()
+
+# %%
+headers = [
+    "symboling",
+    "normalized-losses",
+    "make",
+    "fuel-type",
+    "aspiration",
+    "num-of-doors",
+    "body-style",
+    "drive-wheels",
+    "engine-location",
+    "wheel-base",
+    "length",
+    "width",
+    "height",
+    "curb-weight",
+    "engine-type",
+    "num-of-cylinders",
+    "engine-size",
+    "fuel-system",
+    "bore",
+    "stroke",
+    "compression-ratio",
+    "horsepower",
+    "peak-rpm",
+    "city-mpg",
+    "highway-mpg",
+    "price",
+]
+
+df.columns = headers
+
+# %%
+path = "/Users/felixgyabaa/Documents/data_analysis/car_price_prediction/dataset/Automobile.csv"
+df.to_csv(path)
+
+# %%
+df.replace("?", np.nan, inplace=True)
+
+# %%
+missing_data = df.isnull()
+
+# %%
+missing_data
+
+# %%
+for column in missing_data:
+    print(f"{column} : {missing_data[column].value_counts()}")
+
+# %%
+df["normalized-losses"] = df["normalized-losses"].replace(
+    np.nan, df["normalized-losses"].mean()
+)
+
+# %%
+df.dtypes
+# %%
+df["normalized-losses"] = df["normalized-losses"].astype("float")
+
+# %%
+df["stroke"] = df["stroke"].astype("float")
+
+# %%
+df["stroke"] = df["stroke"].replace(np.nan, df["stroke"].mean())
+
+# %%
+df["bore"] = df["bore"].astype("float")
+
+# %%
+df["bore"] = df["bore"].replace(np.nan, df["bore"].mean())
+
+# %%
+df["horsepower"] = df["horsepower"].astype("float")
+
+# %%
+df["horsepower"] = df["horsepower"].replace(np.nan, df["horsepower"].mean())
+
+# %%
+df["peak-rpm"] = df["peak-rpm"].astype("float")
+
+# %%
+df["peak-rpm"] = df["peak-rpm"].replace(np.nan, df["peak-rpm"].mean())
+
+# %%
