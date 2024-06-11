3f3c8d0d11aaa4eae8e6558d13646d12e93ce63a
felixgyabaa <gyabaafelix01@gmail.com>
2024-06-11 17:29:33 +0000

    binning

--- main.py
+++ main.py
@@ -141,3 +141,42 @@ df["width"] = df["width"] / df["width"].max()
 df["length"] = df["length"] / df["length"].max()
 
 # %%
+df["horsepower"] = df["horsepower"].astype(int, copy=True)
+
+# %%
+import matplotlib as plt
+from matplotlib import pyplot
+
+plt.pyplot.hist(df["horsepower"])
+
+# set x/y labels and plot title
+plt.pyplot.xlabel("horsepower")
+plt.pyplot.ylabel("count")
+plt.pyplot.title("horsepower bins")
+
+# %%
+bins = np.linspace(min(df["horsepower"]), max(df["horsepower"]), 4)
+bins
+
+# %%
+group_names = ["Low", "Medium", "High"]
+
+# %%
+df["horsepower-binned"] = pd.cut(
+    df["horsepower"], bins, labels=group_names, include_lowest=True
+)
+
+df[["horsepower", "horsepower-binned"]].head(20)
+
+# %%
+df["horsepower-binned"].value_counts()
+
+# %%
+pyplot.hist(df["horsepower-binned"], bins=3)
+
+# set x/y labels and plot title
+plt.pyplot.xlabel("horsepower")
+plt.pyplot.ylabel("count")
+plt.pyplot.title("horsepower bins")
+
+# %%
